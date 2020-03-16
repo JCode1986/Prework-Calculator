8a39e5e69a90af4cf4c7407fd2023b730641a8cc
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-16 10:47:46 -0700

    basic functionality of methods complete

--- Program.cs
+++ Program.cs
@@ -4,9 +4,57 @@ namespace Prework_Calculator
 {
     class Program
     {
+
+        public static void UserInput()
+        {
+            Console.WriteLine("Welcome to a very basic calculator");
+            Console.WriteLine("Which operation would you like to do?");
+            Console.WriteLine("'a' - add\n's' - subtract\n'm' - multiply\n'd' - divide");
+            string operationInput = Console.ReadLine();
+
+            if (operationInput == "a") Console.WriteLine(Add());
+            if (operationInput == "s") Console.WriteLine(Subtract());
+            if (operationInput == "m") Console.WriteLine(Multiply());
+            if (operationInput == "d") Console.WriteLine(Divide());           
+        }
+        public static string Add()
+        {
+            Console.WriteLine("Enter first number: ");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int numTwo = int.Parse(Console.ReadLine());
+            return $"Sum of {numOne} and {numTwo} is {numOne + numTwo}";
+        }
+
+        public static string Subtract()
+        {
+            Console.WriteLine("Enter first number: ");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int numTwo = int.Parse(Console.ReadLine());
+            return $"Difference of {numOne} and {numTwo} is {numOne - numTwo}";
+        }
+
+        public static string Multiply()
+        {
+            Console.WriteLine("Enter first number: ");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int numTwo = int.Parse(Console.ReadLine());
+            return $"Product of {numOne} and {numTwo} is {numOne * numTwo}";
+        }
+
+        public static string Divide()
+        {
+            Console.WriteLine("Enter first number: ");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int numTwo = int.Parse(Console.ReadLine());
+            return $"Quotient of {numOne} and {numTwo} is {numOne / numTwo}";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserInput();
         }
     }
 }
